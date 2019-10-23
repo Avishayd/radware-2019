45b03c6e3924fe4ddf4b6b7a54c48f0d31d409d1
Reuven M. Lerner <reuven@lerner.co.il>
2019-10-23 11:43:07 +0300

    gitautosync update 88

--- icecream.py
+++ icecream.py
@@ -5,4 +5,7 @@ s1 = Scoop('chocolate')
 s2 = Scoop('vanilla')
 s3 = Scoop('coffee')
 
-print(s1.flavor)
+print(s1.flavor)  # should print "chocolate"
+
+for one_scoop in [s1, s2, s3]:
+    print(one_scoop.flavor)
