e2aab7d7681b5bcaf61ba49197ade72d65033d98
Reuven M. Lerner <reuven@lerner.co.il>
2019-10-23 12:19:56 +0300

    gitautosync update 106

--- order.py
+++ order.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 
+print("A")
+
+
 class Person():
+    print("B")
+
     def __init__(self, name):
+        print("C")
         self.name = name
+
+    print("D")
 
     def greet(self):
         return f"Hello, {self.name}"
 
+    print("E")
+
+
+print("F")
 
 p1 = Person('name1')
 p2 = Person('name2')
