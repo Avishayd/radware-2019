93f823f1497b471760a6539b9d41d1c66541510e
Reuven M. Lerner <reuven@lerner.co.il>
2019-10-23 12:55:10 +0300

    gitautosync update 125

--- icecream.py
+++ icecream.py
@@ -27,7 +27,8 @@ class Bowl():
 
     def add_scoops(self, *args):
         for one_scoop in args:
-            self.scoops.append(one_scoop)
+            if len(self.scoops) < Bowl.max_scoops:
+                self.scoops.append(one_scoop)
 
     def flavors(self):
         # output = []
